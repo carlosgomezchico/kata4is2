4919d62b5e2bccebc4a0845bb1bde13749203d9d
carlosgomezchico <145378795+carlosgomezchico@users.noreply.github.com>
2024-01-21 12:47:58 +0000

    Update DataVisualizer.java

--- src/main/java/DataVisualizer.java
+++ src/main/java/DataVisualizer.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class DataVisualizer {
+package PACKAGE_NAME;
+
+public class DataVisualizer {
+    public void displayHistogram(Map<Integer, Integer> histogram) {
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+        for (Map.Entry<Integer, Integer> entry : histogram.entrySet()) {
+            dataset.addValue(entry.getValue(), "Frequency", entry.getKey());
+        }
+
+        JFreeChart chart = ChartFactory.createBarChart(
+                "Age Histogram", // Chart title
+                "Age", // X-axis label
+                "Frequency", // Y-axis label
+                dataset,
+                PlotOrientation.VERTICAL,
+                false, true, false);
+
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new java.awt.Dimension(560, 367));
+        // Create a JFrame to display the chart
+        javax.swing.JFrame frame = new javax.swing.JFrame();
+        frame.add(chartPanel);
+        frame.setTitle("Data Visualization");
+        frame.setSize(600, 400);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
+
