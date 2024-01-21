bb9f54ba7771d49fc98843ed3e7c997e7a9e2a00
carlosgomezchico <145378795+carlosgomezchico@users.noreply.github.com>
2024-01-21 12:49:05 +0000

    Update DatasetRecord.java

--- src/main/java/DatasetRecord.java
+++ src/main/java/DatasetRecord.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class DatasetRecord {
+package PACKAGE_NAME;
+
+public class DatasetRecord {
+    // Attributes
+    private int age;
+    private String name;
+    private String city;
+
+    // Constructor
+    public DatasetRecord(int age, String name, String city) {
+        this.age = age;
+        this.name = name;
+        this.city = city;
+    }
+
+    // Getters and Setters
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getCity() { return city; }
+    public void setCity(String city) { this.city = city; }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "DatasetRecord{" +
+               "age=" + age +
+               ", name='" + name + '\'' +
+               ", city='" + city + '\'' +
+               '}';
+    }
 }
+
