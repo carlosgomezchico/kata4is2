c8064bb52d968693921bf4350e351bdcdaa8d8dc
carlosgomezchico <145378795+carlosgomezchico@users.noreply.github.com>
2024-01-21 12:46:56 +0000

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Main {
+package PACKAGE_NAME;
+public class Main {
+    public static void main(String[] args) {
+        DatasetLoader loader = new DatasetLoader();
+        List<DatasetRecord> records = loader.loadDataFromDB("ruta/a/tu/baseDeDatos.db");
+
+        DataProcessor processor = new DataProcessor();
+        // Supongamos que queremos un histograma de edades
+        Map<Integer, Integer> histogram = processor.generateAgeHistogram(records);
+
+        DataVisualizer visualizer = new DataVisualizer();
+        visualizer.displayHistogram(histogram);
+    }
 }
+
